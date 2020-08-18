55ed4e0ab704c0c9bd4809a9fec22b243320f33b
Olga Sowinska <olga.sowinska00@gmail.com>
2020-08-18 15:47:33 +0200

    Updates

---
+++ precache-manifest.24954ca908388bb82028bce635a41ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e5f0ceabeb0226fad291f9f9234d71",
+    "url": "/To-do-list-React/index.html"
+  },
+  {
+    "revision": "18a3e427bb7e20569e65",
+    "url": "/To-do-list-React/static/css/main.ffdc0f40.chunk.css"
+  },
+  {
+    "revision": "d25b62e2846f3ce11e70",
+    "url": "/To-do-list-React/static/js/2.06f132d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list-React/static/js/2.06f132d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a3e427bb7e20569e65",
+    "url": "/To-do-list-React/static/js/main.7d7ec40a.chunk.js"
+  },
+  {
+    "revision": "f4b43e042f3c25a8e9c3",
+    "url": "/To-do-list-React/static/js/runtime-main.e2f1747a.js"
+  }
+]);
