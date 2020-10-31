afd586a51fbbc1893dd45b96b3175cfb4269ac3c
Olga Sowińska <olga.sowinska00@gmail.com>
2020-10-31 18:35:24 +0100

    created styled components Span

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import "../Container/style.css";
 
+const Span = styled.span`
+    display: flex;
+    justify-content: center;
+
+    @media (max-width: 466px) {
+        flex-direction: column;
+        align-items: flex-start;
+    }
+`;
+
 const Button = styled.button`
     border: none;
     background-color: white;
@@ -28,10 +38,10 @@ const Buttons = (props) => {
     }
 
     return (
-        <span className="section__span">
+        <Span className="section__span">
             <Button onClick={props.toggleHideDoneTask} className="section__button">{props.hideDoneTasks ? "Pokaż ukończone" : "Ukryj ukończone"}</Button>
             <Button onClick={props.setAllDone} className="section__button" disabled={props.tasks.every(({ done }) => done)}>Ukończ wszystkie</Button>
-        </span>
+        </Span>
     )
 }
 
