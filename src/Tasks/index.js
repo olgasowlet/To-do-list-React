9c69bce3d9b4ff94c2d509d2d362fc1148e704a0
Olga Sowińska <olga.sowinska00@gmail.com>
2020-10-31 21:04:02 +0100

    added some style component to Tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,22 +1,62 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
+import styled, { css } from "styled-components";
+
+const Ul = styled.ul`
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+`;
+
+const Li = styled.li`
+    display: ${({ display }) => display || "flex"};
+    width: 100%;
+    justify-content: space-around;
+    border-bottom: 1px solid #eeeeee;
+`;
+
+const Button = styled.button`
+    margin: 10px;
+    height: 30px;
+    width: 30px;
+    color: white;
+    box-shadow: none;
+    border: none;
+    align-self: center;
+    padding: 10px;
+    display: flex;
+    transition: 0.5s;
+
+    &:hover {
+        opacity: 0.7;
+    }
+
+    ${({ done }) => done && css`
+        background-color: green;
+    `}
+
+    ${({ toDelete }) => toDelete && css`
+        background-color: red;
+    `}
+`;
 
 const Tasks = (props) => (
-    <ul className="listOfTasks">
+    <Ul>
         {props.tasks.map(task => (
-            <li key={task.id} className={`listedTask${task.done && props.hideDoneTasks ? " listedTask--hidden" : ""}`}>
-                <button onClick={() => props.toggleTaskDone(task.id)} className={`listedTask__button listedTask__button--doner`}>
-                    <i className={`fas${task.done ? " fa-check" : "--none fa-check"}`}></i>
-                </button>
-                <span className={`listedTask__content${task.done ? "--done" : ""}`}>
+            <Li display={`${task.done && props.hideDoneTasks ? "none" : ""}`} key={task.id} >
+                <Button done onClick={() => props.toggleTaskDone(task.id)}>
+                    <i className={`fas${task.done ? " fa-check" : "--none fa-check"} `}></i>
+                </Button>
+                <span className={`listedTask__content${task.done ? "--done" : ""} `}>
                     {task.content}
                 </span>
-                <button onClick={() => props.removeTask(task.id)} className="listedTask__button listedTask__button--remover">
+                <Button toDelete onClick={() => props.removeTask(task.id)}>
                     <span className="far fa-trash-alt"></span>
-                </button>
-            </li>
+                </Button>
+            </Li>
         ))}
-    </ul>
+    </Ul>
 );
 
 export default Tasks;
