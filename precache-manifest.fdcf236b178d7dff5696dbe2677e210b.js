7242ee6b6b1fdd8247165baa68e82222242b2e24
Olga Sowińska <olga.sowinska00@gmail.com>
2020-12-06 12:33:31 +0100

    Updates

---
+++ precache-manifest.fdcf236b178d7dff5696dbe2677e210b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7137ab606a6a80af4085e7decfa9ebee",
+    "url": "/To-do-list-React/index.html"
+  },
+  {
+    "revision": "27409c4db0c82faf1267",
+    "url": "/To-do-list-React/static/css/main.ab001f2c.chunk.css"
+  },
+  {
+    "revision": "8c03442a226d7c58456b",
+    "url": "/To-do-list-React/static/js/2.19ebc310.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-do-list-React/static/js/2.19ebc310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27409c4db0c82faf1267",
+    "url": "/To-do-list-React/static/js/main.99d49e75.chunk.js"
+  },
+  {
+    "revision": "f4b43e042f3c25a8e9c3",
+    "url": "/To-do-list-React/static/js/runtime-main.e2f1747a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/To-do-list-React/precache-manifest.ba831f178deb4ac860b0206185b16858.js"
+  "/To-do-list-React/precache-manifest.fdcf236b178d7dff5696dbe2677e210b.js"
 );
 
 self.addEventListener('message', (event) => {
