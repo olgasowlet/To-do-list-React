1347206a648a2c2d30964e1068972e4dc0dbb8b3
Olga Sowińska <olga.sowinska00@gmail.com>
2020-12-18 19:08:16 +0100

    fixed?

--- src/features/tasks/tasksLocalStorage.js
+++ src/features/tasks/tasksLocalStorage.js
@@ -4,5 +4,7 @@ export const saveTasksInLocalStorage = (tasks) => {
     localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 };
 
-export const getTasksFromLocalStorage = () =>
-    JSON.parse(localStorage.getItem(localStorageKey)) || [];
+export const getTasksFromLocalStorage = () => {
+     const tasks = JSON.parse(localStorage.getItem(localStorageKey));
+     return tasks || [];
+};
